21971338790c0f1eb5617cce5e26b99b1f0ffe62
Jun Yokoyama <jun@larus.org>
2017-10-24 19:28:44 +0900

    initial commit
    bundle gem fluent-plugin-redshift-v2

---
+++ lib/fluent/plugin/redshift/v2.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/redshift/v2/version"
+
+module Fluent
+  module Plugin
+    module Redshift
+      module V2
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/redshift/v2/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Redshift
+      module V2
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
